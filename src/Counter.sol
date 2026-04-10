e4a9ec0cf72a4a1154a2f92c3941d144f7e44c78
allen <allenmuhani@gmail.com>
2026-04-10 17:33:09 +0300

    added and tested work flows with arc

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-https://www.youtube.com/watch?v=sas02qSFZ74&t=21972s
+// https://www.youtube.com/watch?v=sas02qSFZ74&t=21972s
+
 contract Counter {
     uint256 public number;
 
